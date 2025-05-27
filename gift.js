76a813cc9663c2fb357d9df3a7815c93b5538978
iklass abdul <iklassabdul@gmail.com>
2025-05-26 17:13:28 -0700

    Hii

--- gift.js
+++ gift.js
@@ -1,39 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const hangoutSelect = document.getElementById("hangout-select");
-  const hangoutOptions = document.getElementById("hangout-options");
   const form = document.getElementById("gift-form");
-  const success = document.getElementById("success-message");
+  const successMessage = document.getElementById("success-message");
+  const hangoutSelect = document.getElementById("hangout");
+  const hangoutOptions = document.getElementById("hangout-options");
 
-  hangoutSelect.addEventListener("change", function () {
-    if (this.value === "Yes") {
+  // Show/hide hangout sub-options
+  hangoutSelect.addEventListener("change", () => {
+    if (hangoutSelect.value === "Yes") {
       hangoutOptions.classList.remove("hidden");
     } else {
       hangoutOptions.classList.add("hidden");
     }
   });
 
+  // Handle Formspree success
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-    const data = new FormData(form);
-    const action = form.getAttribute("action");
 
-    fetch(action, {
+    const formData = new FormData(form);
+
+    fetch(form.action, {
       method: "POST",
-      body: data,
+      body: formData,
       headers: {
-        Accept: "application/json"
-      }
+        Accept: "application/json",
+      },
     })
-    .then(res => {
-      if (res.ok) {
-        form.classList.add("hidden");
-        success.classList.remove("hidden");
-      } else {
-        alert("Something went wrong. Please try again.");
-      }
-    })
-    .catch(() => {
-      alert("There was a network error.");
-    });
+      .then(response => {
+        if (response.ok) {
+          form.reset();
+          form.classList.add("hidden");
+          successMessage.classList.remove("hidden");
+        } else {
+          alert("Oops! Something went wrong. Try again later.");
+        }
+      })
+      .catch(() => {
+        alert("Oops! Something went wrong. Try again later.");
+      });
   });
 });
